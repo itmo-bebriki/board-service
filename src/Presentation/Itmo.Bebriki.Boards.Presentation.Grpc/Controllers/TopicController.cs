de80874b0760d4a45f1cbdbe0bb13f7111e053ab
pchz <pchz@gmail.com>
2025-01-09 01:06:25 +0300

    Feat: добавлен TopicController

--- src/Presentation/Itmo.Bebriki.Boards.Presentation.Grpc/Controllers/TopicController.cs
+++ src/Presentation/Itmo.Bebriki.Boards.Presentation.Grpc/Controllers/TopicController.cs
@@ -1,32 +1,73 @@
 using Grpc.Core;
+using Itmo.Bebriki.Boards.Application.Contracts.Topics;
+using Itmo.Bebriki.Boards.Application.Contracts.Topics.Commands;
+using Itmo.Bebriki.Boards.Application.Contracts.Topics.Dtos;
+using Itmo.Bebriki.Boards.Presentation.Grpc.Converters.Topics.Requests;
+using Itmo.Bebriki.Boards.Presentation.Grpc.Converters.Topics.Responses;
 using Itmo.Bebriki.Topics.Contracts;
 
 namespace Itmo.Bebriki.Boards.Presentation.Grpc.Controllers;
 
 public sealed class TopicController : TopicService.TopicServiceBase
 {
-    public override Task<QueryTopicResponse> QueryTopic(QueryTopicRequest request, ServerCallContext context)
+    private readonly ITopicService _topicService;
+
+    public TopicController(ITopicService topicService)
     {
-        return base.QueryTopic(request, context);
+        _topicService = topicService;
     }
 
-    public override Task<CreateTopicResponse> CreateTopic(CreateTopicRequest request, ServerCallContext context)
+    public override async Task<QueryTopicResponse> QueryTopic(QueryTopicRequest request, ServerCallContext context)
     {
-        return base.CreateTopic(request, context);
+        QueryTopicCommand internalCommand = QueryTopicRequestConverter.ToInternal(request);
+
+        PagedTopicDto internalResponse =
+            await _topicService.QueryTopicAsync(internalCommand, context.CancellationToken);
+
+        QueryTopicResponse response = QueryTopicResponseConverter.FromInternal(internalResponse);
+
+        return response;
     }
 
-    public override Task<UpdateTopicResponse> UpdateTopic(UpdateTopicRequest request, ServerCallContext context)
+    public override async Task<CreateTopicResponse> CreateTopic(CreateTopicRequest request, ServerCallContext context)
     {
-        return base.UpdateTopic(request, context);
+        CreateTopicCommand internalCommand = CreateTopicRequestConverter.ToInternal(request);
+
+        long internalResponse = await _topicService.CreateTopicAsync(internalCommand, context.CancellationToken);
+
+        CreateTopicResponse response = CreateTopicResponseConverter.FromInternal(internalResponse);
+
+        return response;
     }
 
-    public override Task<AddTopicTasksResponse> AddTopicTasks(SetTopicTasksRequest request, ServerCallContext context)
+    public override async Task<UpdateTopicResponse> UpdateTopic(UpdateTopicRequest request, ServerCallContext context)
     {
-        return base.AddTopicTasks(request, context);
+        UpdateTopicCommand internalCommand = UpdateTopicRequestConverter.ToInternal(request);
+
+        await _topicService.UpdateTopicAsync(internalCommand, context.CancellationToken);
+
+        return new UpdateTopicResponse();
     }
 
-    public override Task<RemoveTopicTasksResponse> RemoveTopicTasks(SetTopicTasksRequest request, ServerCallContext context)
+    public override async Task<AddTopicTasksResponse> AddTopicTasks(
+        SetTopicTasksRequest request,
+        ServerCallContext context)
     {
-        return base.RemoveTopicTasks(request, context);
+        SetTopicTasksCommand internalCommand = SetTopicTasksRequestConverter.ToInternal(request);
+
+        await _topicService.AddTopicTasksAsync(internalCommand, context.CancellationToken);
+
+        return new AddTopicTasksResponse();
+    }
+
+    public override async Task<RemoveTopicTasksResponse> RemoveTopicTasks(
+        SetTopicTasksRequest request,
+        ServerCallContext context)
+    {
+        SetTopicTasksCommand internalCommand = SetTopicTasksRequestConverter.ToInternal(request);
+
+        await _topicService.RemoveTopicTasksAsync(internalCommand, context.CancellationToken);
+
+        return new RemoveTopicTasksResponse();
     }
 }
